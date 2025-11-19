ae6d7e60340ba9d762d9ff874e80c748449709ac
Alexander <gastec139@gmail.com>
2025-11-19 21:20:29 +0300

    update version to 10

--- mod_info.lua
+++ mod_info.lua
@@ -1,11 +1,11 @@
 name = "Select spawn locations"
-uid = "select-spawn-locations-v09"
-version = 9
+uid = "select-spawn-locations-v010"
+version = 10
 copyright = ""
 description = [[Mod that makes you select your ACU spawn location before game starts.
 Best to play on Generated maps with lots of mexes.
 Choose option in lobby for type of spawn areas (=====Select Spawn Location=====): Spawn areas type .
-Otherswise you will get an error or unfair spawn positions.]]
+Otherwise you will get an error or unfair spawn positions.]]
 author = "4z0t, Markers from Eternal-"
 icon = ""
 selectable = true
